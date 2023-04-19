c8587f14fce6916ce565b782d87f690a89f4adb5
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-19 11:32:07 -0400

    Add another user for testing

--- data/user-test-data.js
+++ data/user-test-data.js
@@ -1,4 +1,5 @@
 export default {travelers: [
   {id: 1, name: "Ham Leadbeater", travelerType: "relaxer"},
-  {id: 2, name: "Rachael Vaughten", travelerType: "thrill-seeker"}
+  {id: 2, name: "Rachael Vaughten", travelerType: "thrill-seeker"},
+  {id: 3, name: "Sandy Cheeks", travelerType: "Rootin-tootin-thrill-seeker"}
 ]};
