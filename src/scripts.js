c70a90e33fd3082bb2b9d3d55671da6a5d59bbd8
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-19 14:40:25 -0400

    Provide code to satisfy MVP of displaying user trip info to the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -5,10 +5,55 @@ import './css/styles.css';
 import User from './User';
 import { fetchAllData } from './apiCalls';
 
+/* Query Selectors */
+const pastTrips = document.getElementById("pastTripsBox"),
+      pendingTrips = document.getElementById("pendingTripsBox"),
+      totalCost = document.getElementById("displayPrice"),
+      userName = document.getElementById("username");
+
+/* Global Variables */
+let user; 
+
+/* DOM manipulation */
+const displayData = () => {
+  displayPast();
+  displayUpcoming();
+  displayYearCost();
+  displayName();
+}
+
+const displayPast = () => {
+  user.tripData.forEach(trip => {
+    pastTrips.innerHTML += `<h4>${trip.itinerary.destination}</h4>`;
+  });
+}
+
+const displayUpcoming = () => {
+  if (!user.upcomingDestinations.length) {
+    pendingTrips.innerHTML += `<h4>You have no pending trips</h4>`;
+  }
+  user.upcomingDestinations.forEach(trip => {
+    pendingTrips.innerHTML += `<h4>${trip.itinerary.destination}</h4>`;
+  });
+}
+
+const displayYearCost = () => {
+  totalCost.innerText = `$${user.totalCost}`
+}
+
+const displayName = () => {
+  userName.innerText = ` ${user.name}`
+}
+/* Event Listeners */
 window.addEventListener('load', () => {
   fetchAllData()
   .then(data => {
-    console.log(data)
+    user = new User(data[0].travelers[0])
+    user.findTrips(data[1].trips)
+    user.addItineraries(data[2].destinations)
+    user.getUpcomingTrips();
+    user.getTotalCost("2021");
+    displayData();
   })
 });
 
