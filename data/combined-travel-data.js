cf93e5b7c0e21d78a43cd6cd592c3d731bfb43d3
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-19 11:31:30 -0400

    Initial commit

---
+++ data/combined-travel-data.js
@@ -0,0 +1,57 @@
+export default { userItinerary: [
+  {
+    id: 1,
+    userID: 1,
+    destinationID: 1,
+    travelers: 1,
+    date: '2022/09/16',
+    duration: 8,
+    status: 'approved',
+    suggestedActivities: [],
+    itinerary: {
+      id: 1,
+      destination: 'Lima, Peru',
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 400,
+      image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+      alt: 'overview of city buildings with a clear sky'
+    }
+  },
+  {
+    id: 3,
+    userID: 1,
+    destinationID: 3,
+    travelers: 4,
+    date: '2022/05/22',
+    duration: 17,
+    status: 'approved',
+    suggestedActivities: [],
+    itinerary: {
+      id: 3,
+      destination: 'Sydney, Austrailia',
+      estimatedLodgingCostPerDay: 130,
+      estimatedFlightCostPerPerson: 950,
+      image: 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'opera house and city buildings on the water with boats'
+    }
+  },
+  {
+    id: 5,
+    userID: 1,
+    destinationID: 5,
+    travelers: 3,
+    date: '2022/04/30',
+    duration: 18,
+    status: 'approved',
+    suggestedActivities: [],
+    itinerary: {
+      id: 5,
+      destination: 'Madrid, Spain',
+      estimatedLodgingCostPerDay: 150,
+      estimatedFlightCostPerPerson: 650,
+      image: 'https://images.unsplash.com/photo-1543785734-4b6e564642f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'city with clear skys and a road in the day time'
+    }
+  }
+]
+}
