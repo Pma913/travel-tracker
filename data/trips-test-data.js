62ef0152d4621b50dfa584e6b84cb28adbe41357
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-18 20:30:46 -0400

    User class test updated and initial commit on destination data

---
+++ data/trips-test-data.js
@@ -0,0 +1,62 @@
+export default { trips: [
+  {
+  id:1, 
+  userID: 1, 
+  destinationID: 49,
+  travelers: 1,
+  date: "2022/09/16",
+  duration: 8,
+  status: "approved",
+  suggestedActivities: []
+  },
+  {
+  id: 2,
+  userID: 2,
+  destinationID: 25,
+  travelers: 5,
+  date: "2022/10/04",
+  duration: 18,
+  status: "approved",
+  suggestedActivities: []
+  },
+  {
+  id: 3,
+  userID: 1,
+  destinationID: 22,
+  travelers: 4,
+  date: "2022/05/22",
+  duration: 17,
+  status: "approved",
+  suggestedActivities: []
+  },
+  {
+  id: 4,
+  userID: 2,
+  destinationID: 14,
+  travelers: 2,
+  date: "2022/02/25",
+  duration: 10,
+  status: "approved",
+  suggestedActivities: []
+  },
+  {
+  id: 5,
+  userID: 1,
+  destinationID: 29,
+  travelers: 3,
+  date: "2022/04/30",
+  duration: 18,
+  status: "approved",
+  suggestedActivities: []
+  },
+  {
+  id: 6,
+  userID: 2,
+  destinationID: 35,
+  travelers: 3,
+  date: "2022/06/29",
+  duration: 9,
+  status: "approved",
+  suggestedActivities: []
+  }
+]};
