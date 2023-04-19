eb41b5fc4317726edb86ae56e3a1485cf2880096
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-19 12:20:28 -0400

    Establish a connection to server data using fetch api

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,16 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
+
+import './css/styles.css';
+import User from './User';
+import { fetchAllData } from './apiCalls';
+
+window.addEventListener('load', () => {
+  fetchAllData()
+  .then(data => {
+    console.log(data)
+  })
+});
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
