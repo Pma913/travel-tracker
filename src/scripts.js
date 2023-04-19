b8cd8932ff9dc8a3572f02ae253dcc02d49f21b3
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-19 18:21:35 -0400

    Add functionality to travel form which includes recording input value using that data in a POST making a GET and updating dom with updated data and also adding functionality for on the spot price calculation

--- src/scripts.js
+++ src/scripts.js
@@ -4,15 +4,26 @@
 import './css/styles.css';
 import User from './User';
 import { fetchAllData } from './apiCalls';
+import { postTrip } from './apiCalls';
+import { fetchTrips } from './apiCalls';
 
 /* Query Selectors */
 const pastTrips = document.getElementById("pastTripsBox"),
       pendingTrips = document.getElementById("pendingTripsBox"),
       totalCost = document.getElementById("displayPrice"),
-      userName = document.getElementById("username");
+      userName = document.getElementById("username"),
+      dateInput = document.getElementById("date"),
+      daysInput = document.getElementById("numberOfDays"),
+      travelersInput = document.getElementById("numberOfTravelers"),
+      destinationInput = document.getElementById("destination"),
+      bookButton = document.getElementById("userInputBtn"),
+      tripPrice = document.getElementById("newTripPrice"),
+      inputs = document.querySelectorAll(".data-input");
 
 /* Global Variables */
-let user; 
+let user,
+    tripId,
+    destinations; 
 
 /* DOM manipulation */
 const displayData = () => {
@@ -44,6 +55,73 @@ const displayYearCost = () => {
 const displayName = () => {
   userName.innerText = ` ${user.name}`
 }
+
+const clearDisplay = () => {
+  pastTrips.innerHTML = ``;
+  pendingTrips.innerHTML = ``;
+  userName.innerText = ``;
+  totalCost.innerText = ``;
+}
+
+/* Data manipulators */
+const getPrice = () => {
+  let selectedDestination = destinations.find(dest => {
+    return dest.destination === destinationInput.value;
+  });
+
+  let dayPrice = selectedDestination.estimatedLodgingCostPerDay * daysInput.value;
+
+  let travelerPrice = selectedDestination.estimatedFlightCostPerPerson * travelersInput.value;
+
+  let agentFee = (dayPrice + travelerPrice) * 0.1;
+  let total = dayPrice + travelerPrice + agentFee;
+
+  tripPrice.innerText = `${total}`;
+}
+
+const formatDate = () => {
+  let splitDate = dateInput.value.split('');
+  splitDate.forEach((num, index) => {
+    if (num === '-') {
+      splitDate.splice(index, 1, "/");
+    }
+  })
+  return splitDate.join('');
+}
+
+const addData = () => {
+  let selectedDestination = destinations.find(dest => {
+    return dest.destination === destinationInput.value;
+  });
+
+  let tripData = {
+    id: tripId, 
+    userID: user.id, 
+    destinationID: selectedDestination.id, 
+    travelers: parseInt(travelersInput.value), 
+    date: formatDate(), 
+    duration: parseInt(daysInput.value), 
+    status: 'pending',
+    suggestedActivities: []
+  }
+
+  postTrip(tripData)
+  .then(data => {
+    console.log('post data: ',data)
+    
+    fetchTrips()
+    .then(data => {
+      user.findTrips(data.trips)
+      user.addItineraries(destinations)
+      user.getUpcomingTrips();
+      user.getTotalCost("2021");
+      clearDisplay();
+      displayData();
+      tripId = data.trips.length + 1;
+    })
+  })
+};
+
 /* Event Listeners */
 window.addEventListener('load', () => {
   fetchAllData()
@@ -54,8 +132,20 @@ window.addEventListener('load', () => {
     user.getUpcomingTrips();
     user.getTotalCost("2021");
     displayData();
+    tripId = data[1].trips.length + 1;
+    destinations = data[2].destinations;
   })
 });
 
+bookButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  addData();
+});
 
-console.log('This is the JavaScript entry file - your code begins here.');
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if (destinationInput.value) {
+      getPrice();
+    }
+  });
+});
