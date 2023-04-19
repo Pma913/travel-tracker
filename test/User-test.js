cc085d36eac0987746b44a5a747190fe01a1417e
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-19 11:33:11 -0400

    Add updates to user test to accurately test User class

--- test/User-test.js
+++ test/User-test.js
@@ -3,80 +3,98 @@ const expect = chai.expect;
 import User from '../src/User.js';
 import trips from '../data/trips-test-data.js';
 import travelers from '../data/user-test-data.js';
+import destinations from '../data/destination-test-data.js';
+import userItinerary from '../data/combined-travel-data.js';
 
 describe('User', () => {
 
+  let user;
+  let user2;
+  let user3;
+  let users;
+  let userTrips;
+  let allTrips;
+  let travelLocations;
+  let userTravelLocations;
+  let itinerary;
+
   beforeEach(() => {
-    let users = travelers.travelers;
-    let user = new User(users[0]);
-    let user2 = new User(users[1]);
-    let userTrips = trips.trips.filter(trip => trip.userID === 1);
+    users = travelers.travelers;
+    user = new User(users[0]);
+    user2 = new User(users[1]);
+    user3 = new User(users[2]);
+    userTrips = trips.trips.filter(trip => trip.userID === 1);
+    allTrips = trips.trips;
+    travelLocations = destinations.destinations;
+    userTravelLocations = travelLocations.filter(dest => {
+      return dest.id === 1 || dest.id === 3 || dest.id === 5;
+    });
+    itinerary = userItinerary.userItinerary
+    user.findTrips(allTrips);
+    user2.findTrips(allTrips);
+    user.addItineraries(travelLocations);
+    user2.addItineraries(travelLocations);
   });
 
   it('should be a function', () => {
     expect(User).to.be.a('function');
   });
 
-  it.skip('should be an instance of User', () => {
+  it('should be an instance of User', () => {
     expect(user).to.be.instanceof(User);
   });
 
-  it.skip('should have an id', () => {
+  it('should have an id', () => {
     expect(user.id).to.equal(1)
   });
 
-  it.skip('should have a name', () => {
+  it('should have a name', () => {
     expect(user.name).to.equal("Ham Leadbeater")
   });
 
-  it.skip('should have a default property of traveled', () => {
-    expect(user.traveled).to.equal(undefined);
-  });
-
-  it.skip('should have a property of totalCost with an initial value', () => {
+  it('should have a property of totalCost with an initial value', () => {
     expect(user.totalCost).to.equal(0)
   });
 
-  it.skip('should have a default property of destinations', () => {
+  it('should have a default property of destinations', () => {
     expect(user.destinations).to.equal(undefined);
   });
 
-  it.skip('should have a method of to find past trips', () => {
-    user.findTrips();
-    expect(user.traveled).to.deep.equal(userTrips)
+  it('should have a method of to find past trips', () => {
+    expect(user.tripData).to.deep.equal(userTrips)
   });
 
-  it.skip('should not return error if there are no trips', () => {
-    expect(user.findTrips()).to.equal("You have not taken any trips.")
+  it('should return error if there are no trips', () => {
+    expect(user3.findTrips(allTrips)).to.equal("You have not taken any trips.")
   });
 
-  it.skip('should hava a method to retrieve total cost', () => {
+  it('should have a method to add trip itineraries', () => {
+    expect(user.tripData).to.deep.equal(itinerary)
+  });
+  
+  it('should return error if no trip itineraries', () => {
+    expect(user3.findTrips(travelLocations)).to.equal("You have not taken any trips.")
+  });
+
+  it('should hava a method to retrieve total cost', () => {
     user.getTotalCost("2022");
     expect(user.totalCost).to.equal(12782);
     user2.getTotalCost("2022");
-    expect(user.totalCost).to.equal(6270)
+    expect(user2.totalCost).to.equal(6270)
   })
 
-  it.skip('should hava return error if no trips have been taken', () => {
-    expect(user.getTotalCost("2023")).to.equal("You have not taken any trips yet this year.");
+  it('should hava return error if no trips have been taken', () => {
+    expect(user.getTotalCost("2023", travelLocations)).to.equal("You have not taken any trips yet this year.");
   });
 
-  it.skip('should hava a method to retrieve upcoming trips', () => {
-    let futureTrip = {
-      id: 6,
-      userID: 2,
-      destinationID: 6,
-      travelers: 3,
-      date: "2023/06/29",
-      duration: 9,
-      status: "approved",
-      suggestedActivities: []
-    };
+  it('should hava a method to retrieve upcoming trips', () => {
+    let upcoming = allTrips[5];
+    upcoming.itinerary = travelLocations[5];
     user2.getUpcomingTrips();
-    expect(user2.destinations).to.equal(futureTrip)
+    expect(user2.upcomingDestinations).to.deep.equal([upcoming])
   });
 
-  it.skip('should return an error if there are no upcoming trips', () => {
+  it('should return an error if there are no upcoming trips', () => {
     expect(user.getUpcomingTrips()).to.equal("You have no upcoming trips.")
   });
 
