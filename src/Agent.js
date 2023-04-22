295ba6b43c697552b3d9776c897260ff294d689e
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-04-22 16:49:48 -0400

    Create agent class according to my test suit

--- src/Agent.js
+++ src/Agent.js
@@ -1,20 +1,48 @@
 class Agent {
-  constructor(trips) {
-    this.trips;
+  constructor(trips, locations, users) {
+    this.date = new Date().toJSON().slice(0,10).split('-').join('/');
+    this.trips = trips;
+    this.locations = locations;
     this.newTrips;
     this.totalIncome;
     this.todaysTrips;
+    this.users = users
+    this.client;
   }
 
-  getTripRequests()
+  getTripRequests() {
+    this.newTrips = this.trips.filter(trip => trip.status === 'pending')
+  }
 
-  getTotalIncome()
+  getTotalIncome() {
+    let splitDate = this.date.split('/')
+    splitDate.splice(0,1,splitDate[0] - 1)
+    let lastYear = splitDate.join('/')
+    let pastYearTrips = this.trips.filter(trip => trip.date > lastYear)
+  
+    let price = pastYearTrips.reduce((acc, trip) => {
+      let location = this.locations.find(dest => dest.id === trip.destinationID);
+      let day = location.estimatedLodgingCostPerDay * trip.duration;
+      let travel = location.estimatedFlightCostPerPerson * trip.travelers;
+      acc += (day + travel);
+      return acc 
+    },0)
 
-  getTodaysTrips()
+    this.totalIncome = price * 0.1;
+  }
 
-  approveTrip(tripNumber)
+  getTodaysTrips() {
+    this.todaysTrips = this.trips.filter(trip => trip.date === this.date)
+  }
 
-  deleteTrip(tripNumber)
+  locateTrip(tripNum) {
+    return this.newTrips.find(trip => trip.id === tripNum)
+  }
 
-  findUser(userName)
+  findUser(userName) {
+    this.client = this.users.find(user => user.name === userName)
+    console.log(this.client)
+  }
 }
+
+export default Agent;
